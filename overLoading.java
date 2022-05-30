66a505ddb144d581534c1a8dc41ba5d7d5bda89f
egesezgun <egesezgun@checkbox.com.tr>
2022-05-30 21:32:49 +0300

    overloading
    overloading trying

---
+++ overLoading.java
@@ -0,0 +1,20 @@
+public class overLoading {
+
+    static void print (){
+        System.out.println("Parametre olmadan metot");
+    }
+    static void print(int a){
+        System.out.println("Parametreler :" + a);
+    }
+    static int print(int b , int a){
+        return a+b;
+    }
+    
+    public static void main(String[] args) {
+        print();
+        print(29);
+        System.out.println(print(2,3));
+
+    }
+
+}
